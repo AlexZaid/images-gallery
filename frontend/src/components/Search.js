e5ca56bbf8997b66d9f03c778a458fe1995e0f69
Manuel <alex.cuentauni@gmail.com>
2022-06-25 11:23:18 -0500

    search style input

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -3,18 +3,20 @@ import { Container,Row,Col,Form,Button } from "react-bootstrap";
 
 const Search = () => {
     return(
-            <Container>
-              <Row>
-                <Col><Form>
+            <Container className="mt-4">
+              <Row className="justify-content-center">
+                <Col xs={12} md={8} lg={6}>
+                    <Form>
                         <Row className="align-items-center">
-                              <Col sm={3} className="my-1">
+                              <Col xs={9} className="my-1">
                                 <Form.Control id="inlineFormInputName" placeholder="Search" />
                               </Col>
                               <Col xs="auto" className="my-1">
                                 <Button type="submit">Search</Button>
                               </Col>
-                            </Row>
-                    </Form></Col>
+                         </Row>
+                    </Form>
+                </Col>
               </Row>
             </Container>
     );
