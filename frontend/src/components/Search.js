89a0dd4882e7ad041a45c0b5fbd472d5f28823f9
Manuel <alex.cuentauni@gmail.com>
2022-06-25 16:10:14 -0500

    pretty format

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,31 +1,31 @@
 import React from "react";
-import { Container,Row,Col,Form,Button } from "react-bootstrap";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
 
-const Search = ({word, setWord, handleSubmit}) => {
-    return(
-            <Container className="mt-4">
-              <Row className="justify-content-center">
-                <Col xs={12} md={8} lg={6}>
-                    <Form onSubmit={handleSubmit}>
-                        <Row className="align-items-center">
-                              <Col xs={9} className="my-1">
-                                <Form.Control 
-                                  type="text"
-                                  value={word}
-                                  onChange={(e)=> setWord(e.target.value)}
-                                  id="inlineFormInputName"                                 
-                                  placeholder="Search" 
-                                />
-                              </Col>
-                              <Col xs="auto" className="my-1">
-                                <Button type="submit">Search</Button>
-                              </Col>
-                        </Row>
-                    </Form>
-                </Col>
-              </Row>
-            </Container>
-    );
+const Search = ({ word, setWord, handleSubmit }) => {
+  return (
+    <Container className="mt-4">
+      <Row className="justify-content-center">
+        <Col xs={12} md={8} lg={6}>
+          <Form onSubmit={handleSubmit}>
+            <Row className="align-items-center">
+              <Col xs={9} className="my-1">
+                <Form.Control
+                  type="text"
+                  value={word}
+                  onChange={(e) => setWord(e.target.value)}
+                  id="inlineFormInputName"
+                  placeholder="Search"
+                />
+              </Col>
+              <Col xs="auto" className="my-1">
+                <Button type="submit">Search</Button>
+              </Col>
+            </Row>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Search;
