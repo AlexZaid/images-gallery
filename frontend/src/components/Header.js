c13143772f7690acf31927d6fb4f7f4dfbbbec94
Manuel <alex.cuentauni@gmail.com>
2022-06-25 03:35:50 -0500

    NavBar Style

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { Navbar,Nav,NavDropdown,Form,FormControl,Button,Container } from 'react-bootstrap';
 
+const navbarStyle={
+    backgroundColor: 'black'
+
+};
+
 const Header = ({title}) => {
     return (
-        <Navbar bg="dark" variant="dark" expand="lg">
+        <Navbar style={navbarStyle}  variant="dark" expand="lg">
             <Container fluid>
               <Navbar.Brand href="#">{title}</Navbar.Brand>
               <Navbar.Toggle aria-controls="navbarScroll" />
@@ -15,27 +20,7 @@ const Header = ({title}) => {
                 >
                   <Nav.Link href="#action1">Home</Nav.Link>
                   <Nav.Link href="#action2">Link</Nav.Link>
-                  <NavDropdown title="Link" id="navbarScrollingDropdown">
-                    <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action5">
-                      Something else here
-                    </NavDropdown.Item>
-                  </NavDropdown>
-                  <Nav.Link href="#" disabled>
-                    Link
-                  </Nav.Link>
                 </Nav>
-                <Form className="d-flex">
-                  <FormControl
-                    type="search"
-                    placeholder="Search"
-                    className="me-2"
-                    aria-label="Search"
-                  />
-                  <Button variant="outline-success">Search</Button>
-                </Form>
               </Navbar.Collapse>
             </Container>
         </Navbar>
